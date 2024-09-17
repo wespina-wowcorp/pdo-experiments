11565f6ca17fd17690746cee77f9777268bd2ab2
wilson espina <wilson.espina@woolworths.co.nz>
2024-09-17 15:10:03 +1200

    AB-167: Adds base tests

---
+++ AB-167/Variant-1/main.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         AB-167: Variant 1
+// @namespace    https://woolworths-agile.atlassian.net/browse/AB-167
+// @version      AB-167_variant_1
+// @description  Cartology Personalization - Pet Segment
+// @author       Wilson
+// @match        https://www.woolworths.co.nz/
+// @match        https://www.woolworths.co.nz/lists/myfavourites
+// @require      file://C:/Users/1442718/Development/overrides/AB-167/Variant-1/main.js
+// @grant        GM_addStyle
+// ==/UserScript==
+
+document.documentElement.dataset.webAb167 = "1";
---
+++ AB-167/Variant-2/main.js
@@ -0,0 +1,15 @@
+
+  // ==UserScript==
+  // @name         AB-167: Variant 2
+  // @namespace    https://woolworths-agile.atlassian.net/browse/AB-167
+  // @version      AB-167_variant_2
+  // @description  Cartology Personalization - Pet Segment
+  // @author       Wilson
+  // @match        https://www.woolworths.co.nz/
+  // @match        https://www.woolworths.co.nz/lists/myfavourites
+  // @require      file://C:/Users/1442718/Development/overrides/AB-167/Variant-2/main.js
+  // @grant        GM_addStyle
+  // ==/UserScript==
+
+  document.documentElement.dataset.webAb167 = "2";
+  
